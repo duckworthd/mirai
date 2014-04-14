00fc3775eca9261932b7f09d20591fde84303e46
Daniel Duckworth <duckworthd@gmail.com>
2014-04-13 21:09:37 -0700

    dependency versions in requirements.txt, setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@ import mirai
 
 
 setup(
-    name = 'mirai',
-    version = mirai.__version__,
-    author = 'Daniel Duckworth',
-    author_email = 'duckworthd@gmail.com',
-    description = "Twitter Futures in Python",
-    license = 'BSD',
-    keywords = 'futures concurrent finagle twitter',
-    url = 'http://github.com/duckworthd/mirai',
-    packages = find_packages(),
-    classifiers = [
+    name              = 'mirai',
+    version           = mirai.__version__,
+    author            = 'Daniel Duckworth',
+    author_email      = 'duckworthd@gmail.com',
+    description       = "Twitter Futures in Python",
+    license           = 'BSD',
+    keywords          = 'futures concurrent finagle twitter',
+    url               = 'http://github.com/duckworthd/mirai',
+    packages          = find_packages(),
+    classifiers       = [
       'Development Status :: 4 - Beta',
       'License :: OSI Approved :: BSD License',
       'Operating System :: OS Independent',
       'Programming Language :: Python',
     ],
-    install_requires = [
-      "futures",
+    install_requires  = [
+      "futures>=2.1.6",
     ],
-    test_requires = [
-      "nose",
+    test_requires     = [
+      "nose>=1.3.1",
     ]
 )
