ab3b8180c4567fba0adbdfa9d6b61cf1bb86a1dd
Daniel Duckworth <dux@premise.com>
2014-05-10 19:27:56 -0700

    find requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,36 +1,44 @@
 from setuptools import setup, find_packages
 
-def version():
+def version(name):
   import os
   folder   = os.path.split(__file__)[0]
-  mirai    = os.path.join(folder, "mirai")
+  mirai    = os.path.join(folder, name)
   _version = os.path.join(mirai, "_version.py")
   env      = {}
   execfile(_version, env)
   return env['__version__']
 
-print find_packages()
-setup(
-    name              = 'mirai',
-    version           = version(),
-    author            = 'Daniel Duckworth',
-    author_email      = 'duckworthd@gmail.com',
-    description       = "Twitter Futures in Python",
-    license           = 'BSD',
-    keywords          = 'futures concurrent finagle twitter',
-    url               = 'http://github.com/duckworthd/mirai',
-    packages          = find_packages(),
-    classifiers       = [
-      'Development Status :: 4 - Beta',
-      'License :: OSI Approved :: BSD License',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-    ],
-    install_requires  = [
-      "setuptools>=3.4.4",
-      "futures>=2.1.6",
-    ],
-    test_requires     = [
-      "nose>=1.3.1",
-    ]
-)
+
+def requirements():
+  import os
+  folder       = os.path.split(__file__)[0]
+  requirements = os.path.join(folder, "requirements.txt")
+  with open(requirements, 'r') as f:
+    lines = f.readlines()
+    lines = [l.strip().split("#")[0] for l in lines]
+  return lines
+
+
+if __name__ == '__main__':
+  setup(
+      name              = 'mirai',
+      version           = version('mirai'),
+      author            = 'Daniel Duckworth',
+      author_email      = 'duckworthd@gmail.com',
+      description       = "Twitter Futures in Python",
+      license           = 'BSD',
+      keywords          = 'futures concurrent finagle twitter',
+      url               = 'http://github.com/duckworthd/mirai',
+      packages          = find_packages(),
+      classifiers       = [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      install_requires  = requirements(),
+      tests_require     = [
+        "nose>=1.3.1",
+      ]
+  )
